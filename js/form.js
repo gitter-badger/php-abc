b7c50593dc1b88fe6992fda30288e834f203df69
Paul Water <p.r.water@setbased.nl>
2014-01-17 08:57:07 +0100

    Added JavaScript for SET forms.

---
+++ js/form.js
@@ -0,0 +1,46 @@
+/*jslint browser: true, vars: true, indent: 2, maxlen: 120 */
+/*global window */
+/*global $ */
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Object constructor.
+ *
+ * @param {jQuery} $form The jQuery object of the SET form.
+ * @constructor
+ */
+function SET_Form($form) {
+  "use strict";
+  var that = this;
+
+  this.$myFrom = $form;
+
+  // Install event handlers.
+  this.$myFrom.submit(function () {
+    // Note: When a form control is disabled the browser will not send the value of the input to web server and
+    // the Set Based form classes will assume that the values of these form controls have changed. To prevent this
+    // we enable all disabled form controls such that the browser will send the value of disabled form controls.
+    that.$myFrom.find(':disabled').prop('disabled', false);
+  });
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Array with all registered SET forms.
+ *
+ * @type {{SET_Form}}
+ */
+SET_Form.ourForms = [];
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Registers forms that match the jQuery selector as a SET form.
+ */
+SET_Form.registerForm = function (selector) {
+  "use strict";
+  $(selector).find('form').each(function () {
+    SET_Form.ourForms[SET_Form.ourForms.length] = new SET_Form($(this));
+  });
+};
+
+// ---------------------------------------------------------------------------------------------------------------------
