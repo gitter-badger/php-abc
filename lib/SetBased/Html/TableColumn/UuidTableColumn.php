7931eced4d847b79b8928c7e32924cb4115e9be1
Valery Zuban <zubey.next@gmail.com>
2014-05-24 01:13:48 +0300

    TAB-3 UUID Column.

---
+++ lib/SetBased/Html/TableColumn/UuidTableColumn.php
@@ -0,0 +1,40 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Html\TableColumn;
+
+use SetBased\Html\Html;
+
+//----------------------------------------------------------------------------------------------------------------------
+class UuidTableColumn extends TableColumn
+{
+  /**
+   * The field name of the data row used for generating this table column.
+   *
+   * @var string
+   */
+  protected $myFieldName;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param string $theHeaderText The header text this table column.
+   * @param string $theFieldName  The field name of the data row used for generating this table column.
+   */
+  public function __construct( $theHeaderText, $theFieldName )
+  {
+    $this->myDataType   = 'uuid';
+    $this->myHeaderText = $theHeaderText;
+    $this->myFieldName  = $theFieldName;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  public function getHtmlCell( &$theData )
+  {
+    return '<td>'.Html::txt2Html( $theData[$this->myFieldName] ).'</td>';
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
