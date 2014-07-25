6d11e70098923093cc2069aa6f26053643804065
Paul Water <p.r.water@setbased.nl>
2014-07-25 14:50:37 +0200

    Add column type for HTML snippets.

---
+++ lib/Form/Control/HtmlControl.php
@@ -0,0 +1,100 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Html\Form\Control;
+
+use SetBased\Html\Html;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Class HtmlControl
+ *
+ * @package SetBased\Html\Form\Control
+ */
+class HtmlControl extends Control
+{
+  /**
+   * @var string The HTML code.
+   */
+  protected $myHtml;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the HTML code for this form control.
+   *
+   * @param string $theParentName
+   *
+   * @return string
+   */
+  public function generate( $theParentName )
+  {
+    $html = $this->myPrefix;
+    $html .= $this->myHtml;
+    $html .= $this->myPostfix;
+
+    return $html;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns null.
+   */
+  public function getSubmittedValue()
+  {
+    return null;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Set the inner HTML of this span element.
+   *
+   * @param string $theHtmlSnippet The inner HTML. It is the developer's responsibility to unsure it is valid HTML code.
+   */
+  public function setHtml( $theHtmlSnippet )
+  {
+    $this->myHtml = $theHtmlSnippet;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Set the inner text of this span element.
+   *
+   * @param string $theText The inner text. This text will be converted to valid HTML code.
+   */
+  public function setInnerText( $theText )
+  {
+    $this->myHtml = HTML::txt2Html( $theText );
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @param mixed $theValues
+   */
+  public function setValuesBase( &$theValues )
+  {
+    // Nothing to do.
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * {@inheritdoc}
+   */
+  protected function loadSubmittedValuesBase( &$theSubmittedValue, &$theWhiteListValue, &$theChangedInputs )
+  {
+    // Nothing to do.
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * @param array $theInvalidFormControls
+   *
+   * @return bool
+   */
+  protected function validateBase( &$theInvalidFormControls )
+  {
+    return true;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
