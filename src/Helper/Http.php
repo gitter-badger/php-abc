c8d64c7e75926d3734768c93f972484243228176
Paul Water <p.r.water@setbased.nl>
2015-10-29 13:31:04 +0100

    Added constant HTTP_BAD_REQUEST.

--- src/Helper/Http.php
+++ src/Helper/Http.php
@@ -10,6 +10,7 @@ class Http
 {
   //--------------------------------------------------------------------------------------------------------------------
   const HTTP_MOVED_PERMANENTLY = 301;
+  const HTTP_BAD_REQUEST = 400;
   const HTTP_NOT_FOUND = 404;
   const HTTP_INTERNAL_SERVER_ERROR = 500;
 
