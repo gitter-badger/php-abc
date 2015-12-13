ac396e5e8b02b00220934b37275b13e8a31754b3
Paul Water <p.r.water@setbased.nl>
2015-12-13 12:14:22 +0100

    Harmonizing column names and types.

--- lib/ddl/0100_create_tables.sql
+++ lib/ddl/0100_create_tables.sql
@@ -1,11 +1,11 @@
 /*================================================================================*/
 /* DDL SCRIPT                                                                     */
 /*================================================================================*/
-/*  Title    : Abc                                                          */
-/*  FileName : abc.ecm                                                      */
+/*  Title    : ABC Framework                                                      */
+/*  FileName : framework.ecm                                                      */
 /*  Platform : MySQL 5                                                            */
 /*  Version  : Concept                                                            */
-/*  Date     : zondag 27 september 2015                                           */
+/*  Date     : zondag 13 december 2015                                            */
 /*================================================================================*/
 /*================================================================================*/
 /* CREATE TABLES                                                                  */
@@ -14,7 +14,7 @@
 CREATE TABLE `AUT_COMPANY` (
   `cmp_id` SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
   `cmp_abbr` VARCHAR(15) NOT NULL,
-  `cmp_label` VARCHAR(20) CHARACTER SET ascii NOT NULL,
+  `cmp_label` VARCHAR(20) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`cmp_id`)
 );
 
@@ -67,7 +67,7 @@ CREATE TABLE `AUT_CONFIG` (
   `cfg_show_to_company` BOOL DEFAULT 0 NOT NULL,
   `cfg_modify_by_company` BOOL DEFAULT 0 NOT NULL,
   `cfg_description` VARCHAR(400) NOT NULL,
-  `cfg_label` VARCHAR(48) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `cfg_label` VARCHAR(50) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`cfg_id`)
 )
 engine=innodb;
@@ -98,7 +98,7 @@ engine=innodb;
 CREATE TABLE `BBL_WORD_GROUP` (
   `wdg_id` TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
   `wdg_name` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `wdg_label` VARCHAR(32) CHARACTER SET ascii,
+  `wdg_label` VARCHAR(30) CHARACTER SET ascii COLLATE ascii_general_ci,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`wdg_id`)
 );
 
@@ -107,7 +107,7 @@ CREATE TABLE `BBL_WORD` (
   `wdg_id` TINYINT UNSIGNED NOT NULL,
   `wrd_modified` DATETIME NOT NULL,
   `wrd_comment` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `wrd_label` VARCHAR(48) CHARACTER SET ascii,
+  `wrd_label` VARCHAR(50) CHARACTER SET ascii COLLATE ascii_general_ci,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`wrd_id`),
   CONSTRAINT `wrd_label` UNIQUE (`wrd_label`)
 );
@@ -183,14 +183,14 @@ engine=innodb;
 CREATE TABLE `AUT_LOGIN_RESPONSE` (
   `lgr_id` TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
   `wrd_id` SMALLINT UNSIGNED NOT NULL,
-  `lgr_label` VARCHAR(40) CHARACTER SET ascii NOT NULL,
+  `lgr_label` VARCHAR(40) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`lgr_id`)
 );
 
 CREATE TABLE `AUT_PAGE_TAB` (
   `ptb_id` TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
   `wrd_id` SMALLINT UNSIGNED NOT NULL,
-  `ptb_label` VARCHAR(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `ptb_label` VARCHAR(30) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`ptb_id`)
 )
 engine=innodb;
@@ -202,7 +202,7 @@ CREATE TABLE `AUT_PAGE` (
   `mnu_id` SMALLINT UNSIGNED,
   `wrd_id` SMALLINT UNSIGNED NOT NULL,
   `pag_class` VARCHAR(128) NOT NULL,
-  `pag_label` VARCHAR(128) CHARACTER SET ascii,
+  `pag_label` VARCHAR(128) CHARACTER SET ascii COLLATE ascii_general_ci,
   `pag_weight` INT,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`pag_id`)
 );
@@ -295,7 +295,7 @@ CREATE TABLE `DEV_UUID_KEY` (
 
 CREATE TABLE `ELM_ATTRIBUTE` (
   `mat_id` TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
-  `mat_label` VARCHAR(32) CHARACTER SET ascii NOT NULL,
+  `mat_label` VARCHAR(30) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`mat_id`)
 );
 
@@ -313,7 +313,7 @@ CREATE TABLE `LOG_SESSION` (
 CREATE TABLE `LOG_EVENT_TYPE` (
   `let_id` TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
   `wrd_id` SMALLINT UNSIGNED NOT NULL,
-  `let_label` VARCHAR(48) CHARACTER SET ascii NOT NULL,
+  `let_label` VARCHAR(50) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`let_id`)
 )
 engine=innodb;
@@ -339,7 +339,7 @@ CREATE TABLE `LOG_LOGIN` (
   `lgr_id` TINYINT UNSIGNED NOT NULL,
   `cmp_id` SMALLINT UNSIGNED,
   `usr_id` INTEGER UNSIGNED,
-  `llg_date_time` DATETIME NOT NULL,
+  `llg_datetime` DATETIME NOT NULL,
   `llg_user_name` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `llg_company_abbr` VARCHAR(15) CHARACTER SET ascii NOT NULL,
   CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`lli_id`)
@@ -486,7 +486,7 @@ CREATE INDEX `cmp_id` ON `LOG_LOGIN` (`cmp_id`);
 
 CREATE INDEX `lgr_id` ON `LOG_LOGIN` (`lgr_id`);
 
-CREATE INDEX `llg_date_time` ON `LOG_LOGIN` (`llg_date_time`);
+CREATE INDEX `llg_date_time` ON `LOG_LOGIN` (`llg_datetime`);
 
 CREATE INDEX `llg_user_name` ON `LOG_LOGIN` (`llg_user_name`);
 
