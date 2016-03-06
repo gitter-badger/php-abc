25382eda7b4457bbebaee8d44aaf54efd0c531d3
Paul Water <p.r.water@setbased.nl>
2016-03-06 14:28:53 +0100

    Show errors in louver controls.

--- src/Form/Control/SlatControl.php
+++ src/Form/Control/SlatControl.php
@@ -12,6 +12,12 @@ class SlatControl extends ComplexControl
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
+   * @var Control;
+   */
+  private $myDeleteControl;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
    * {@inheritdoc}
    */
   public function generate()
@@ -35,6 +41,71 @@ class SlatControl extends ComplexControl
   }
 
   //--------------------------------------------------------------------------------------------------------------------
+  public function setDeleteControl($theControl)
+  {
+    $this->myDeleteControl = $theControl;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Executes a validators on the child form controls of this form complex control. If  and only if all child form
+   * controls are valid the validators of this complex control are executed.
+   *
+   * @param array $theInvalidFormControls A nested array of invalid form controls.
+   *
+   * @return bool True if and only if all form controls are valid.
+   */
+  public function validateBase(&$theInvalidFormControls)
+  {
+    $valid = true;
+
+    if ($this->myDeleteControl)
+    {
+      if (!$this->myDeleteControl->validateBase($theInvalidFormControls))
+      {
+        $this->myInvalidControls[] = $this->myDeleteControl;
+        $valid                     = false;
+      }
+      else
+      {
+        if ($this->myDeleteControl->getSubmittedValue())
+        {
+          return $valid;
+        }
+      }
+    }
+
+    // First, validate all child form controls.
+    foreach ($this->myControls as $control)
+    {
+      if ($control!==$this->myDeleteControl)
+      {
+        if (!$control->validateBase($theInvalidFormControls))
+        {
+          $this->myInvalidControls[] = $control;
+          $valid                     = false;
+        }
+      }
+    }
+
+    if ($valid)
+    {
+      // All the child form controls are valid. Validate this complex form control.
+      foreach ($this->myValidators as $validator)
+      {
+        $valid = $validator->validate($this);
+        if ($valid!==true)
+        {
+          $theInvalidFormControls[] = $this;
+          break;
+        }
+      }
+    }
+
+    return $valid;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
   /**
    * Returns a table cell with the errors messages of all form controls at this row.
    *
@@ -55,6 +126,10 @@ class SlatControl extends ComplexControl
         $ret .= '<br/>';
       }
       $ret .= '</td>';
+    }
+    else
+    {
+      $ret .= '<td class="error"></td>';
     }
 
     return $ret;
